842077e99da23c707ec323e70b2d42521502b7bc
Jack Reynolds <jackreynolds9000@gmail.com>
2018-01-05 15:46:33 -0500

    Setting Up Basic Scene, Highlighting Movement
    Mirrors will be up soon guys... I'm watching unity tutorials XD

---
+++ Assets/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	/**generic management class for all one-off initialization and heavy-lifting stuff**/
+	/**Stuff like locking cursor, initializing scene, should go in here**/
+
+	// Use this for initialization
+	void Start () {
+		Cursor.lockState = CursorLockMode.Locked;
+		
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
